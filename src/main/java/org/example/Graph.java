f429006817f6f4e70b949f074a5f40a0319fb7cf
ahollingsworth8064 <ahollingsworth8064@student.pps.net>
2025-04-17 09:47:44 -0700

    steal code https://stackoverflow.com/questions/10767265/drawing-a-line-on-a-jframe

--- src/main/java/org/example/Graph.java
+++ src/main/java/org/example/Graph.java
@@ -1,6 +1,7 @@
 package org.example;
 
 import javax.swing.*;
+import javax.xml.crypto.Data;
 import java.awt.*;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -15,6 +16,9 @@ public class Graph {
     private int screenHeight;
     private JFrame screen;
     private Panels panel;
+    private int xGraphTicks;
+    private int yGraphTicks;
+
 
 
     public Graph(double xAxis, double yaxis, double step){
@@ -33,11 +37,13 @@ public class Graph {
         createScreen();
 
     }
-    public Graph(double yAxis, double xAxis, double step, Point[] data, int screenWidth, int screenHeight){
+    public Graph(double yAxis, double xAxis, double step, int screenWidth, int screenHeight,Tuple<Double, Double>[] data){
         this.xAxis = xAxis;
         this.yAxis = yAxis;
         this.step =  step;
-        this.points.addAll(Arrays.asList(data));
+        for(Tuple cord: data){
+            this.points.add(new Point((int)cord.x,(int)cord.y));
+        }
         this.screenHeight = screenHeight;
         this.screenWidth = screenWidth;
         createScreen();
@@ -61,6 +67,9 @@ public class Graph {
 
 
     private void createScreen(){
+        this.xGraphTicks = (int)(screenWidth -100)/10;
+        this.yGraphTicks = (int)(screenHeight -100)/10;
+
         System.out.println("no");
         this.panel = new Panels();
 
@@ -70,81 +79,46 @@ public class Graph {
 
         this.screen.add(this.panel);
 
-        this.screen.setSize(500,500);
+        this.screen.setSize(this.screenWidth,this.screenHeight);
         this.screen.setVisible(true);
 
 
     }
 
     private class Point{
-        double xCor;
-        double yCor;
-        Color color;
+        public double xCor;
+        public double yCor;
+        private double reltiveX;
+        private double reltiveY;
+
+        public Color color;
         public Point(double xCor, double yCor){
             this.xCor = xCor;
             this.yCor = yCor;
+            this.reltiveX = (Graph.this.screenWidth - 100)/step;
             this.color = Color.black;
-
-
         }
 
     }
-}
-class Panels extends JPanel{
 
+    class Panels extends JPanel{
 
-    public void paintComponent(Graphics g) {
-            System.out.println("ees");
+        public void paintComponent(Graphics g) {
             super.paintComponent(g);
+            int yAxisXcor = Graph.this.screenHeight-50;
+            int xAxisYCor =Graph.this.screenHeight-50;
 
-            int width = getWidth();
-            int height = getHeight();
-
-            int drawCounter = 0; // counters for all the while statements
-            int drawCounter2 = 0;
-            int drawCounter3 = 0;
-            int drawCounter4 = 0;
+            g.drawLine(50, xAxisYCor, Graph.this.screenWidth-50,Graph.this.screenHeight-50 );
+            g.drawLine(50, 25, 50,Graph.this.screenHeight-50 );//y axis
 
 
-            int x1 = 0; // cords change with the while statemetns
-            int x2 = 0;
-            int y1 = 0;
-            int y2 = 0;
-            while (drawCounter <= 15) { // counter
-                y2 = 250;
-                g.drawLine(x1, y1, x2, y2);
-                x2 = x2 + 15;
-                y1 = y1 + 15;
-                drawCounter++;
+            for(int i  = 50 + xGraphTicks; i < Graph.this.screenWidth-50; i += xGraphTicks){
+                g.drawLine(i,xAxisYCor -2 , i, xAxisYCor + 2 );
             }
 
 
-            int u1 = 0;
-            int u2 = 0;
-            int v1 = 0;
-            int v2 = 0;
-            while (drawCounter2 <= 15) {
-                u2 = 250;
-                g.drawLine(u1, v1, u2, v2);
-                u1 = u1 + 15;
-                v2 = v2 + 15;
-                drawCounter2++;
-            }
-
-            int a1 = 0;
-            int a2 = 0;
-            int b1 = 0;
-            int b2 = 0;
-
-            while (drawCounter3 <= 15) {
-                a2 = 250;
-                g.drawLine(a1, b1, a2, b2);
-                b1 = b1 + 15;
-                a2 = a2 - 15;
-                drawCounter3++;
-
-            }
         }
+    }
 }
 
 
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,8 +1,7 @@
 package org.example;
-
 public class Main {
     public static void main(String[] args) {
-        Graph e = new Graph(500,500,10, 500,500);
+        Graph e = new Graph(500,500,10, 500,500) ;
 
 
 }}
