4ad61b31524f0aaab0eb4ac7eb3134763e1c447a
ahollingsworth8064 <ahollingsworth8064@student.pps.net>
2025-04-17 09:13:38 -0700

    steal code https://stackoverflow.com/questions/10767265/drawing-a-line-on-a-jframe

--- src/main/java/org/example/Graph.java
+++ src/main/java/org/example/Graph.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Graph extends JPanel {
+public class Graph {
     public double xAxis;
     public double yAxis;
     public double step;
@@ -14,7 +14,7 @@ public class Graph extends JPanel {
     private int screenWidth;
     private int screenHeight;
     private JFrame screen;
-    private panel panels;
+    private Panels panel;
 
 
     public Graph(double xAxis, double yaxis, double step){
@@ -22,6 +22,7 @@ public class Graph extends JPanel {
         this.yAxis = yaxis;
         this.step =  step;
         createScreen();
+
 
     }
     public Graph(double yAxis, double xAxis, double step, Point[] data){
@@ -60,19 +61,22 @@ public class Graph extends JPanel {
 
 
     private void createScreen(){
-        this.panels = new panel();
+        System.out.println("no");
+        this.panel = new Panels();
 
         this.screen = new JFrame("Graph");
-        this.screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.screen.add(this.panels);
 
-        this.screen.setSize(new Dimension(this.screenWidth, this.screenHeight));
+        this.screen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        this.screen.add(this.panel);
+
+        this.screen.setSize(500,500);
         this.screen.setVisible(true);
 
 
     }
 
-    class Point{
+    private class Point{
         double xCor;
         double yCor;
         Color color;
@@ -85,15 +89,62 @@ public class Graph extends JPanel {
         }
 
     }
-    static class panel extends JPanel{
-        @Override
-        public void paintComponents(Graphics g) {
-            super.paintComponent(g);
-            g.setColor(Color.black);
-            g.drawLine(0, 0,0,0);
-
-        }
-    }
-
-
 }
+class Panels extends JPanel{
+
+
+    public void paintComponent(Graphics g) {
+            System.out.println("ees");
+            super.paintComponent(g);
+
+            int width = getWidth();
+            int height = getHeight();
+
+            int drawCounter = 0; // counters for all the while statements
+            int drawCounter2 = 0;
+            int drawCounter3 = 0;
+            int drawCounter4 = 0;
+
+
+            int x1 = 0; // cords change with the while statemetns
+            int x2 = 0;
+            int y1 = 0;
+            int y2 = 0;
+            while (drawCounter <= 15) { // counter
+                y2 = 250;
+                g.drawLine(x1, y1, x2, y2);
+                x2 = x2 + 15;
+                y1 = y1 + 15;
+                drawCounter++;
+            }
+
+
+            int u1 = 0;
+            int u2 = 0;
+            int v1 = 0;
+            int v2 = 0;
+            while (drawCounter2 <= 15) {
+                u2 = 250;
+                g.drawLine(u1, v1, u2, v2);
+                u1 = u1 + 15;
+                v2 = v2 + 15;
+                drawCounter2++;
+            }
+
+            int a1 = 0;
+            int a2 = 0;
+            int b1 = 0;
+            int b2 = 0;
+
+            while (drawCounter3 <= 15) {
+                a2 = 250;
+                g.drawLine(a1, b1, a2, b2);
+                b1 = b1 + 15;
+                a2 = a2 - 15;
+                drawCounter3++;
+
+            }
+        }
+}
+
+
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,10 +1,8 @@
 package org.example;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-
 public class Main {
     public static void main(String[] args) {
         Graph e = new Graph(500,500,10, 500,500);
+
 
 }}
