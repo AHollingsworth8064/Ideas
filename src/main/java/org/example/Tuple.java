258bec4119698718b84ad601fed021c8bf6b07b0
ahollingsworth8064 <ahollingsworth8064@student.pps.net>
2025-05-28 09:57:00 -0700

    finish test

--- src/main/java/org/example/Tuple.java
+++ src/main/java/org/example/Tuple.java
@@ -1,11 +1,26 @@
 package org.example;
 
+import java.util.Objects;
+
 public class Tuple<X,Y>{
-    public final X x;
-    public final Y y;
+    public X x;
+    public Y y;
 
     public Tuple( X x, Y y) {
         this.x = x;
         this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if(obj.getClass().isInstance(this)){
+            return (((Tuple<?, ?>) obj).x.equals(this.x) && ((Tuple<?, ?>) obj).y.equals(this.y));
+        }
+        return false;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
     }
 }
--- src/main/java/org/example/visuals/StupidIdea.java
+++ src/main/java/org/example/visuals/StupidIdea.java
@@ -4,6 +4,7 @@ import org.example.Tuple;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.ArrayList;
 import java.util.HashMap;
 
 
@@ -12,6 +13,7 @@ public class StupidIdea extends JButton{
     private int xCor, yCor, height,width;
     private Color color;
     private String text;
+    private static Tuple<String, String> callerID;
     private static final HashMap<Tuple<String, String>, ButtonPanel> panel = new HashMap<>();
 
     public StupidIdea(int xCor, int yCor, Color color, String text, int width, int height){
@@ -28,6 +30,7 @@ public class StupidIdea extends JButton{
         setLocation(xCor,yCor);
         setText(this.text);
         setBackground(this.color);
+
 
 
 
@@ -64,21 +67,13 @@ public class StupidIdea extends JButton{
         this.setText(this.text);
     }
 
-    public static String getCaller(){
+    public static Tuple<String, String> getCaller(){
         StackTraceElement[] thing = Thread.currentThread().getStackTrace();
         for(StackTraceElement stack: thing){
             System.out.println("File :" + stack.getFileName()+ ", Class: " + stack.getClassName() + " , Method: " +stack.getMethodName()  + ", Line : " +stack.getLineNumber());
-            if(!stack.getClassName().equals(StupidIdea.class.getName()) && !stack.getClassName().equals(java.lang.Thread.class.getName()) ){
-                return stack.getClassName();
-            }
-        }
-        return null;
-    }
-    public static Tuple<String, String> getCaller(Object object){
-        StackTraceElement[] thing = Thread.currentThread().getStackTrace();
-        for(StackTraceElement stack: thing){
-            System.out.println("File :" + stack.getFileName()+ ", Class: " + stack.getClassName() + " , Method: " +stack.getMethodName()  + ", Line : " +stack.getLineNumber());
-            if(!stack.getClassName().equals(StupidIdea.class.getName()) && !stack.getClassName().equals(java.lang.Thread.class.getName()) ){
+            if(!stack.getClassName().equals(StupidIdea.class.getName()) && !stack.getClassName().equals(java.lang.Thread.class.getName()) ){//checking to see if its this calling it or the thread
+
+
                 return new Tuple<>(stack.getFileName(), stack.getMethodName());
             }
         }
@@ -86,28 +81,59 @@ public class StupidIdea extends JButton{
         return null;
     }
 
+    public static Tuple<String, String> getCaller(Object object){
+        StackTraceElement[] thing = Thread.currentThread().getStackTrace();
+        boolean found = false;
+
+        for(StackTraceElement stack: thing){
+            System.out.println("File :" + stack.getFileName()+ ", Class: " + stack.getClassName() + " , Method: " +stack.getMethodName()  + ", Line : " +stack.getLineNumber());
+            
+//            if(!stack.getClassName().equals(StupidIdea.class.getName()) && !stack.getClassName().equals(java.lang.Thread.class.getName()) ){
+//                return new Tuple<>(stack.getFileName(), stack.getMethodName());
+//            }
+        }
+
+        return null;
+    }
 
     public static void setUp(JFrame jframe){
         panel.put(getCaller(jframe), new ButtonPanel());
-        //jframe.add(panel.get(getCaller(jframe)));
+        jframe.add(panel.get(getCaller(jframe)));
     }
 
     public static void setUp(JPanel jPanel){
         panel.put(getCaller(jPanel), new ButtonPanel());
-        //jPanel.add(panel.get(getCaller()));
+        jPanel.add(panel.get(getCaller(jPanel)));
     }
 
     private static class ButtonPanel extends JPanel {
+        private Tuple<String, String> panelID;
+
         public ButtonPanel() {
             setLayout(new GridLayout());
         }
 
-
+        public static Tuple<String, String> getPanelID(){
+            String first = " ", second = "";
+            Tuple<String,String> callerID = new Tuple<>(first, second) ;
+            return callerID;
+        }
 
         @Override
         public void paintComponents(Graphics g) {
             super.paintComponents(g);
         }
+
+    }
+    public static class Maybe extends JLayeredPane{
+        private HashMap<Tuple<String, String>,Maybe> allPanels;
+
+        public Maybe(ButtonPanel buttons, JPanel mainPanel){
+            this.add(mainPanel, 1);
+            this.add(buttons, 0);
+           // allPanels.put();
+        }
+
 
     }
 
