01fbbd83614d8999bb0784badea81c5a634860df
ahollingsworth8064 <ahollingsworth8064@student.pps.net>
2025-06-03 09:55:29 -0700

    I can finaly access the code of a java file from inside the code!! Yay
    now try doing a inline method to allow all objects to call a method

--- src/main/java/org/example/test/objectB.java
+++ src/main/java/org/example/test/objectB.java
@@ -1,21 +1,19 @@
 package org.example.test;
 
+import java.io.File;
+
 public class objectB {
     public int test;
     public static int id;
     private objectA p;
 
-    public objectB(int num) {
-        test = num;
-        id++;
-        this.p = new objectA(3);
-        this.p.check(this);
-        //test
-    }
+
 
     public void test() {
         System.out.println(this.test);
-
+        File test = new File("nope");
+        System.out.println(test.exists()
+        );
     }
 
 
